a6e5af120a4f91a2dc713dbaa7156d4b2fd360cc
PanSzelescik <panszelescik@gmail.com>
2021-12-05 23:07:15 +0100

    Use faster iteration

--- php/api/files.php
+++ php/api/files.php
@@ -24,76 +24,102 @@ if (!is_dir($dir)) {
         'path' => $_SERVER['PATH_INFO']
     ]);
 }
+$rows = getFilesInfo($_SERVER['PATH_INFO']);
+try {
+    $dir_it = new RecursiveDirectoryIterator($dir);
+    foreach ($dir_it as $info) {
+        $name = $info->getFilename();
+        if ($name === '.' or $name === '..') {
+            continue;
+        }
 
-if ($dh = opendir($dir)) {
-    $rows = getFilesInfo($_SERVER['PATH_INFO']);
-    while (($name = readdir($dh)) != false) {
-        if ($name !== '.' and $name !== '..') {
-            $path = "$dir/$name";
+        $path = "$dir/$name";
 
-            $modified = @filemtime($path);
-            if ($modified === false) {
-                $modified = -1;
+        $modified = getMTime($info);
+        if ($modified === false) {
+            $modified = -1;
+        }
+
+        if ($info->isDir()) {
+            $dirs[] = [
+                'name' => $name,
+                'modified' => $modified
+            ];
+        } else {
+            $db_name = "{$_SERVER['PATH_INFO']}/$name";
+            $size = getSize($info);
+            if ($size === false) {
+                $size = 0;
             }
 
-            if (is_dir($path)) {
-                $dirs[] = [
+            $cached_row = false;
+            $row = @$rows[$db_name];
+            if ($row and $row['modified'] === $modified) {
+                $cached_row = $row;
+            }
+
+            if ($cached_row) {
+                $files[] = [
                     'name' => $name,
-                    'modified' => $modified
+                    'modified' => $modified,
+                    'mime' => $cached_row['mime'],
+                    'size' => $size
                 ];
             } else {
-                $db_name = "{$_SERVER['PATH_INFO']}/$name";
-                $size = @filesize($path);
-                if ($size === false) {
-                    $size = 0;
-                }
+                $mime = getMime($path);
 
-                $cached_row = false;
-                $row = @$rows[$db_name];
-                if ($row && $row['modified'] === $modified) {
-                    $cached_row = $row;
-                }
+                $files[] = [
+                    'name' => $name,
+                    'modified' => $modified,
+                    'mime' => $mime,
+                    'size' => $size
+                ];
 
-                if ($cached_row) {
-                    $files[] = [
-                        'name' => $name,
-                        'modified' => $modified,
-                        'mime' => $cached_row['mime'],
-                        'size' => $size
-                    ];
-                } else {
-                    $mime = @mime_content_type($path);
-                    if ($mime === false) {
-                        $mime = 'unknown';
-                    }
-
-                    $files[] = [
-                        'name' => $name,
-                        'modified' => $modified,
-                        'mime' => $mime,
-                        'size' => $size
-                    ];
-
-                    $to_insert[$db_name] = [
-                        'modified' => $modified,
-                        'mime' => $mime,
-                        'size' => $size
-                    ];
-                }
-            };
+                $to_insert[$db_name] = [
+                    'modified' => $modified,
+                    'mime' => $mime,
+                    'size' => $size
+                ];
+            }
         }
     }
-    closedir($dh);
     echo json_encode([
         'files' => $files,
         'dirs' => $dirs,
         'path' => substr($_SERVER['PATH_INFO'], 1)
     ]);
     insertFilesInfo($to_insert);
-} else {
+} catch (Exception) {
     http_response_code(500);
     echo json_encode([
         'message' => "Directory can't be opened",
         'path' => $_SERVER['PATH_INFO']
     ]);
+}
+
+function getMTime($info) {
+    try {
+        return $info->getMTime();
+    } catch (Exception) {
+    }
+    return -1;
+}
+
+function getSize($info) {
+    try {
+        return $info->getSize();
+    } catch (Exception) {
+    }
+    return -1;
+}
+
+function getMime($path) {
+    try {
+        $mime = @mime_content_type($path);
+        if ($mime) {
+            return $mime;
+        }
+    } catch (Exception) {
+    }
+    return 'unknown';
 }
